4490de890059914a67c333274f0728a40510a7d1
saman <hushmandsaman@gmail.com>
2019-12-03 18:54:47 +0330

    first commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const path=require('path')
+const express=require('express')
+var app=express()
+const publicPath=path.join(__dirname,'../public')
+app.use(express.static(publicPath))
+
+app.listen(3000,()=>{
+console.log('saman')
+})
