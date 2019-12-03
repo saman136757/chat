87b6c4a8f76514cc0fce56a9aa72e30f41167e40
saman <hushmandsaman@gmail.com>
2019-12-04 01:41:34 +0330

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const path=require('path')
 const express=require('express')
+const socketIo=require('socket.io')
+const http=require('http')
+
 var app=express()
+var server=http.createServer(app)
+var io=socketIo(server)
+io.on('connection',(socket)=>{
+    console.log('user connection')
+    io.on('disconnect',()=>{
+        console.log('user disconnected')
+    })
+})
+
 const publicPath=path.join(__dirname,'../public')
 app.use(express.static(publicPath))
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
 console.log('saman')
 })
